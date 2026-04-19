f34f44ade6de6060d473f3c0cd42b5af5afcd5d6
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-04-19 23:46:20 +0530

    update

--- 13 Array-3/question1.cpp
+++ 13 Array-3/question1.cpp
@@ -5,20 +5,43 @@
 #include <algorithm>
 using namespace std;
 
-void sort01 (vector<int>& v){
-    int n  = v.size();
-    int noOf1 = 0;
-    int noOf0 = 0;
-    for(int i=0; i<n; i++){
-        if(v[i]==0) noOf0++;
-        else noOf1++;
+// Method 1
+
+// void sort01 (vector<int>& v){
+//     int n  = v.size();
+//     int noOf1 = 0;
+//     int noOf0 = 0;
+//     for(int i=0; i<n; i++){
+//         if(v[i]==0) noOf0++;
+//         else noOf1++;
+//     }
+//     //filling elements
+//     for(int i=0; i<n; i++){
+//         if(i<noOf0) v[i] = 0;
+//         else v[i] = 1;
+//     }
+// }
+
+// Method 2
+
+void sort01m2(vector<int> &v){
+    int n = v.size();
+    int i = 0;
+    int j = n-1;
+    while (i<j){
+        if(v[i]==0) i++;
+        if(v[j]==1) j--;
+        if(i>j) break;
+        if(v[i]==1 && v[j]==0){
+            v[i] = 0;
+            v[j] = 1;
+            i++;
+            j--;
+        }
     }
-    //filling elements
-    for(int i=0; i<n; i++){
-        if(i<noOf0) v[i] = 0;
-        else v[i] = 1;
-    }
+    
 }
+
 int main(){
     vector<int> v;
     v.push_back(1);
@@ -34,16 +57,23 @@ int main(){
     }
     cout<<endl;
 
-    // Method 1
+    // Method
     // sort(v.begin(),v.end());
     // for(int i=0; i<v.size(); i++){
     //     cout<<v[i]<<" ";
     // }
 
+    // Method 1
+    // sort01(v);
+    // for(int i=0; i<v.size(); i++){
+    //     cout<<v[i]<<" ";
+    // }
+
     // Method 2
-    sort01(v);
+    sort01m2(v);
     for(int i=0; i<v.size(); i++){
         cout<<v[i]<<" ";
     }
+
     cout<<endl;
 }
