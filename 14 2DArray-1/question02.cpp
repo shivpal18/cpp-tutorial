15c6634ac07bacf34a64ff6efd409cf550dde7f9
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-04-28 00:00:45 +0530

    update

---
+++ 14 2DArray-1/question02.cpp
@@ -0,0 +1,28 @@
+// Write a C++ program to find the largest element of a given 2D array of integers.
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+    int m;
+    cout<<"Enter the no of rows: ";
+    cin>>m;
+    int n;
+    cout<<"Enter the no of columns: ";
+    cin>>n;
+    int arr[m][n];
+    for(int i=0; i<=m-1; i++){
+        for(int j=0; j<=n-1; j++){
+            cin>>arr[i][j];
+        }
+    }
+    
+    int max = INT_MIN;
+    for(int i=0; i<=m-1; i++){
+        for(int j=0; j<=n-1; j++){
+            if(max<arr[i][j]) max = arr[i][j];
+        }
+    }
+    cout<<max;
+}
