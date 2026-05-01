56b5e33926d0714d1fa2d9de67a5d291f243176a
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-05-01 21:42:44 +0530

    update

--- 14 2DArray-1/question05.cpp
+++ 14 2DArray-1/question05.cpp
@@ -4,5 +4,39 @@
 using namespace std;
 
 int main(){
-    
+    int m;
+    cout<<"Enter the no of rows: ";
+    cin>>m;
+    int n;
+    cout<<"Enter the no of columns: ";
+    cin>>n;
+    int arr[m][n];
+    for(int i=0; i<=m-1; i++){
+        for(int j=0; j<=n-1; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+    // print
+    for(int i=0; i<=m-1; i++){
+        for(int j=0; j<=n-1; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    // store the transpose
+    int t[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            t[i][j] = arr[j][i];
+        }
+    }
+    // printing transpose matrix
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<t[i][j]<<" ";
+        }
+        cout<<endl; 
+    }
 }
