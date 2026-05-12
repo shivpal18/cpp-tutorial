b0281a8c2327cf363954584a38df7d081d94b3ec
shivpal18 <chaurasiysdilip9794@gmail.com>
2026-05-12 23:27:42 +0530

    update

--- Part 15 assignment/question02.cpp
+++ Part 15 assignment/question02.cpp
@@ -11,5 +11,23 @@ int main(){
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++) cin>>a[i][j];
     }
-    
+
+    // step 1 : to find the given matrix
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i<j) swap(a[i][j], a[j][i]);
+        }
+    }
+
+    //  step 2 : reverse each column one by one
+    for(int j=0; j<n; j++){
+        for(int i=0; i<n/2; i++){
+            swap(a[i][j], a[n-i-1][j]);
+        }
+    }
+    cout<<"the rotated array is : "<<endl;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++) cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
 }
